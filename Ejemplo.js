aece9dd2cea441d4567473b1b456e3155621457a
sameoldlove <Gpao1313@gmail.com>
2017-07-12 10:55:55 -0500

    sameoldlove

--- Ejemplo.js
+++ Ejemplo.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+alert (" Hola prros");
+
+ if (confirm( " Estas contento lo mas probable es que no ")){
+	 
+	 alert("me caes mal");
+ }
+ else
+ 
+ {
+	 alert("JAJAJAJAJAJAJAJA ");
+ }
